7a7ba84e6e58248af3c4ef493ac4b8291218d326
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-13 16:40:37 +0100

    Refactored spec

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,5 +3,4 @@ require 'bike'
 describe Bike do 
   #expect Bike.new to respond to the method working?
   it { is_expected.to respond_to(:working?) }
-  it { is_expected.to respond_to(:working?) }
 end
