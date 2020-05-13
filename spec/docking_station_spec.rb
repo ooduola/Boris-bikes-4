bbd16b2cfc71ecbbb5bdb68350517f64b9cd7dff
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-13 16:39:02 +0100

    Added to docking_station spec. With errors

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,4 +3,8 @@ require './lib/docking_station.rb'
 
 describe DockingStation do
     it {is_expected.to respond_to(:release_bike)}
+    it 'release_bike' do
+      station = DockingStation.new
+      expect(station.release_bike).to 
+    end
 end
