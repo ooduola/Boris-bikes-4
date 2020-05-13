ba5546c8b7581655568cd0db0f8e22e2e34f2bc2
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-13 19:18:00 +0100

    Update test for release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,11 +4,7 @@ require './lib/docking_station.rb'
 describe DockingStation do
     it {is_expected.to respond_to(:release_bike)}
     it 'release_bike' do
-        station = DockingStation.new
-        expect(station.release_bike).to eq Bike
-    end
-    it 'bike.working?' do 
-        bike = Bike.new
-        expect(bike.working?).to eq true
+        bike = subject.release_bike
+        expect(bike).to be_working
     end
 end
