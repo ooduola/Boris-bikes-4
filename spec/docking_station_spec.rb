b9d62f79a597e42684b2bb8c6c7939f32bb32b06
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-13 17:53:03 +0100

    Added bike.working? equal to true in docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,10 @@ require './lib/docking_station.rb'
 describe DockingStation do
     it {is_expected.to respond_to(:release_bike)}
     it 'release_bike' do
-      station = DockingStation.new
-      expect(station.release_bike).to eq Bike
+        station = DockingStation.new
+        expect(station.release_bike).to eq Bike
+    end
+    it 'bike.working?' do 
+        expect(Bike.working?).to eq true
     end
 end
