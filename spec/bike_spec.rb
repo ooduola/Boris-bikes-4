2e415e25601c665af1c7cc24f7c37f1c155d3e09
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-13 16:14:25 +0100

    Updated bike class to define working? method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,4 +3,5 @@ require 'bike'
 describe Bike do 
   #expect Bike.new to respond to the method working?
   it { is_expected.to respond_to(:working?) }
+  it { is_expected.to respond_to(:working?) }
 end
