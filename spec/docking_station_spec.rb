e67d9a1a0fb311708346026e65a240d993b2dc65
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-14 14:52:00 +0100

    Added and passed unit test for dock_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,7 @@ describe DockingStation do
         bike = subject.release_bike
         expect(bike).to be_working
     end
+    it 'responds to dock_bike' do
+        expect(subject).to respond_to(:dock_bike)
+    end
 end
