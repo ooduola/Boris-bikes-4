b017d1972baf5a2b8668b1db2baa37946591fb9b
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-15 22:26:20 +0100

    Created unit test for broken method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,5 +2,10 @@ require 'bike'
 
 describe Bike do 
   #expect Bike.new to respond to the method working?
-  it { is_expected.to respond_to(:working?) }
+  it { is_expected.to respond_to(:report_working?) }
+
+  it 'responds to #broken' do
+    expect(subject).to respond_to(:broken)
+  end
+  
 end
