ebc51502b547d90259c7bd238533b10c9c831061
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-15 08:17:21 +0100

    Updated unit test to include specific error messages

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,35 +2,34 @@
 require './lib/docking_station.rb'
 
 describe DockingStation do
-subject(:docking_station) { described_class.new }
 
-    it {is_expected.to respond_to(:release_bike)}
-    #it 'release_bike' do
-        #bike = subject.release_bike
-        #expect(bike).to be_working
-  #  end
-    describe '#release_bike' do
-        it 'releases a bike' do
-            docking_station.dock_bike(Bike.new)
-            expect(docking_station.release_bike).to be_instance_of(Bike)
-        end
+    it 'respond to #release_bike' do
+        expect(subject).to respond_to(:release_bike)
     end
 
     it 'responds to dock_bike' do
-        expect(DockingStation.new).to respond_to(:dock_bike).with(1).argument
+        expect(subject).to respond_to(:dock_bike).with(1).argument
     end
 
     it 'responds to show' do
         expect(subject).to respond_to (:show)
     end
 
-    it 'raise an error when asked to release bike in an empty station' do
-        expect { DockingStation.new.release_bike }.to raise_error
+    describe '#release_bike' do
+        it 'releases a bike' do
+            subject.dock_bike(Bike.new)
+            expect(subject.release_bike).to be_instance_of(Bike)
+        end
+
+        it 'raise specific errop when station is empty' do
+            expect { subject.release_bike }.to raise_error(StopIteration)
+        end
     end
 
-    it 'raise an error when asked to dock a bike in a full station' do
-        20.times { docking_station.dock_bike(Bike.new)}
-        expect { docking_station.dock_bike(Bike.new) }.to raise_error ("No space")
+    describe '#release_bike' do
+        it 'raise specific error when station is full' do
+            20.times { subject.dock_bike(Bike.new)}
+            expect { subject.dock_bike(Bike.new) }.to raise_error(StopIteration)
+        end
     end
-
 end
